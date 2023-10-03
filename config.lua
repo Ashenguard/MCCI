878bb5983e8d5cd3ded19984fe20911b2c364e68
Ashenguard <myminecraft2019@gmail.com>
2023-10-03 19:06:44 +0330

    Better configs

--- config.lua
+++ config.lua
@@ -1,50 +1,36 @@
--- Read the "readme.md" first!
--- https://github.com/Ashenguard/MCCI
-
--- This is a config file you can use to customize the RSWarehouse
 local config = {}
 
--- The storage mod
---    Possible values: "RS", "AE"
---    Note: AE is not implemented yet so...
-config.storage_mod = "RS"
--- Storage container connected to warehouse or entangled block location
-config.export_to = "entangled:tile_2"
--- Time between checks in seconds
-config.internal = 30
+do
+    for line in io.lines("config.ini") do
+        if line:match("(.+)%s+=%s+(.+)") then
+            local key, value = line:match("(.+)%s+=%s+(.+)")
+            if value:match("^%d+$") then
+                value = tonumber(value)
+            end
+
+            config[key] = value
+        end
+    end
+end
 
 
--- Configs related to work requests
-config.work = {}
-
--- Enable work requests scan
-config.work.enable = true
--- Details shown at the right side of work requests
---    Possible values: "Builder", "Stock"
-config.work.details = "Builder"
--- Monitor name to check first, If not found will find and use an unused monitor
-config.work.monitor = nil
-
-
--- Config related to researches
-config.research = {}
-
--- Enable research scan
-config.research.enable = true
--- Show description of researches waiting to be done
-config.research.description = true
--- Show required materials for research (Might need wider monitor)
-config.research.materials = false
--- Monitor name to check first, If not found will find and use an unused monitor
-config.research.monitor = nil
-
-
--- Config related to buildings (WIP)
-config.buildings = {}
--- Enable buildings scan
-config.buildings.enable = true
--- Monitor name to check first, If not found will find and use an unused monitor
-config.buildings.monitor = nil
-
+function config.save()
+    local lines = {}
+    for line in io.lines("config.ini") do
+        if line:match("^(.+)%s+=%s+(.+)$") then
+            local key, _ = line:match("^(.+)%s+=%s+(.+)$")
+            if config[key] ~= nil then
+                line = key .. " = " .. tostring(config[key])
+            end
+        end
+        lines:insert(line)
+    end
+    
+    local file = fs.open("config.ini", "w")
+    for _, line in ipairs(lines) do
+        file.writeLine(line)
+    end
+    file.close()
+end
 
 return config
